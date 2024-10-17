21d7d479ed1b268d0981a58aa1409a854f474887
KaphAein <cherifafadaly@gmail.com>
2024-10-17 13:43:57 +0000

    commit

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- Adding trigger to decrease quantity after inserting into orders
+DELIMITER //
+DROP TRIGGER IF EXISTS validate_email;
+CREATE TRIGGER validate_email
+BEFORE INSERT ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END //
+DELIMITER ;
