300072e6db365eb8b7f0fac4a638086554b8fdce
KaphAein <cherifafadaly@gmail.com>
2024-10-17 11:47:23 +0000

    commit

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- creates a table users
-CREATE TABLE users IF NOT EXIST (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
