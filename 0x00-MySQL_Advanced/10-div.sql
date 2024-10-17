e1919c493ff5674f1cdb5fe2212275cb1573d8c3
KaphAein <cherifafadaly@gmail.com>
2024-10-17 15:17:01 +0000

    commit

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- Function SafeDiv
+DELIMITER //
+
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END //
+
+DELIMITER ;
