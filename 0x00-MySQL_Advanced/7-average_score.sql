f07eb96037206a32c11a0d5a6e0b070a27664094
KaphAein <cherifafadaly@gmail.com>
2024-10-17 14:42:16 +0000

    commit

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,22 +1,22 @@
+-- Average score procedure
 DELIMITER //
-
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+
 BEGIN
     DECLARE total_score INT DEFAULT 0;
     DECLARE project_count INT DEFAULT 0;
     DECLARE average_score FLOAT DEFAULT 0;
 
     SELECT IFNULL(SUM(score), 0)
-    INTO total_score
-    FROM corrections
-    WHERE corrections.user_id = user_id;
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
 
     SELECT COUNT(*)
-    INTO project_count
-    FROM corrections
-    WHERE corrections.user_id = user_id;
+        INTO project_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
 
     IF project_count = 0 THEN
         SET average_score = 0;
@@ -25,8 +25,8 @@ BEGIN
     END IF;
 
     UPDATE users
-    SET average_score = average_score
-    WHERE users.id = user_id;
+        SET average_score = average_score
+        WHERE users.id = user_id;
 
 END //
 
