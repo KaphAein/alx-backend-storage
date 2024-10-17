d2f38135bd9859b66cdc4508bc64c8b2b8310db3
KaphAein <cherifafadaly@gmail.com>
2024-10-17 12:29:38 +0000

    commit

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- creates a table metal fans
 SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
     FROM  metal_bands
-    WHERE style = 'Glam rock'
+    WHERE FIND_IN_SET('Glam Rock', IFNULL(style, '')) > 0
     ORDER BY lifespan DESC;
